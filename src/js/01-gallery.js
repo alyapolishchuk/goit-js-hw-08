f56946c950b82ceb980910576f1dc332951759a2
alyapolishchuk <alinochkarul081@gmail.com>
2022-06-20 21:29:32 +0300

    cc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const div = document.querySelector('.gallery');
+const newGallery = createGallery(galleryItems);
+
+div.insertAdjacentHTML('beforeend', newGallery);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+// new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+//   captionsData: 'alt',
+//   scrollZoom: false,
+//   disableRightClick: true,
+// });
+
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
