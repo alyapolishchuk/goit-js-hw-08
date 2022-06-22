f9f349e908cad102f667d707dbcd015a1b3ece19
alyapolishchuk <alinochkarul081@gmail.com>
2022-06-22 21:34:08 +0300

    ссс

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,31 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const obj = {};
-
 form.addEventListener('submit', formSubmit);
-form.addEventListener('input', formInput);
-
+form.addEventListener('input', throttle(formInput, 500));
+formOnUpdate();
 function formInput(event) {
   obj[event.target.name] = event.target.value; // присвоєння ключа об'єкту через квадратні дужки
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(obj));
-  // console.log(obj);
-  // console.log(event.target.message);
-  // console.dir(event.target.input);
 }
 function formSubmit(event) {
-    event.preventDefault()
-    // console.dir(event.currentTarget.elements.message.value);
-    const { email, message } = event.target.elements;
-    // console.log(email.value, message.value);
-    if (!email.value || !message.value) {
-        alert(`Заповніть форму`);
-    } 
+  event.preventDefault();
+  const { email, message } = event.target.elements;
+  if (!email.value || !message.value) {
+    alert(`Заповніть форму`);
+  }
+  console.log(obj);
+  localStorage.clear();
+  form.reset();
+  obj = {};
 }
-
+function formOnUpdate() {
+  const storageValue = localStorage.getItem(LOCALSTORAGE_KEY);
+  const objectStorageValue = JSON.parse(storageValue);
+  Object.entries(objectStorageValue).forEach(([name, value]) => {
+    obj[name] = value;
+    form.elements[name].value = value;
+  });
+}
